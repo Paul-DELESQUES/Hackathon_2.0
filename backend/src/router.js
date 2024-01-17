37dfc354e7217b554adcbd14026f8fe6dae81af3
FredP21 <f.pelletier21@gmail.com>
2024-01-17 22:15:48 +0100

    fix router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -7,16 +7,13 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
+const productControllers = require("./controllers/productControllers");
 
-// Route to get a list of items
-router.get("/items", itemControllers.browse);
+// Route to get a list of products
+router.get("/products", productControllers.browse);
 
-// Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
-
-// Route to add a new item
-router.post("/items", itemControllers.add);
+// Route to get a specific product by ID
+router.get("/products/:id", productControllers.read);
 
 /* ************************************************************************* */
 
