670dabbe4175aad54ead8d31916c489c8d1a598e
Younes SEFIANI <tokyotweener21@gmail.com>
2024-01-17 22:33:54 +0100

    Création et implantation du composant Questionnaire

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ function Navbar() {
       <Link to="/">Home</Link>
       <Link to="/products">Products</Link>
       <Link to="/shop">Shop</Link>
+      <Link to="/survey">Questionnaire</Link>
     </nav>
   );
 }
