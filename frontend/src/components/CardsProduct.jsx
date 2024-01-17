0afbc2fd69829bd2b7b00faffe49f405a0274b7b
Alexandre-27 <alexandre24102000@gmail.com>
2024-01-17 15:54:25 +0100

    Creation of the CardsProduct.jsx component and the cardsProduct.scss css file. npm i carried out and added the 11 images of l'oréal products

---
+++ frontend/src/components/CardsProduct.jsx
@@ -0,0 +1,10 @@
+function CardsProduct() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>hello</p>
+    </>
+  );
+}
+
+export default CardsProduct;
--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,9 +1,11 @@
+import CardsProduct from "../components/CardsProduct";
 import "../styles/products.scss";
 
 function Products() {
   return (
     <div className="products">
       <h1>Products</h1>
+      <CardsProduct />
     </div>
   );
 }
