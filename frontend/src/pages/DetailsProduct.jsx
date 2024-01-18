592d9f6095996cda6f362975172880546a685f5f
Alexandre-27 <alexandre24102000@gmail.com>
2024-01-18 16:07:17 +0100

    Creating the DetailsProduct.jsx component and adding tags to the component

---
+++ frontend/src/pages/DetailsProduct.jsx
@@ -0,0 +1,30 @@
+function DetailsProduct() {
+  return (
+    <section>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h1>Détails du produit</h1>
+        <h2>Description</h2>
+        <p>
+          La crème hydratante l'oréal hydrabocution est pafaite pour nettoyer
+          les ports du visage
+        </p>
+        <h2>Catégorie du produit</h2>
+        <p>hair</p>
+        <h2>Quantité</h2>
+        <p>28</p>
+      </div>
+      <h2>Produits susceptible de vous plaire</h2>
+      <ul>
+        <li>
+          <img src="" alt="" />
+          <p>Prix: 5.99€</p>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default DetailsProduct;
