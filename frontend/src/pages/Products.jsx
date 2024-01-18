9b6940d19e6308532050dbc4162875f8357d1756
Alexandre-27 <alexandre24102000@gmail.com>
2024-01-18 07:52:15 +0100

    Adding more cards to the CardsProduct.jsx file and adding a class to redefine the position of the cards foundation image in the cardsProduct.scss file

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import VanillaTilt from "vanilla-tilt";
 import "../styles/cardsProduct.scss";
 import lipStick from "../assets/products/rouge_a_levres-removebg-preview.png";
+import foundation from "../assets/products/foundation_oreal-removebg-preview.png";
 
 function Products() {
   useEffect(() => {
@@ -43,6 +44,122 @@ function Products() {
             <h3>Détails Produits</h3>
             <small>Couleur: rouge</small>
             <small>durée: 4 semaines</small>
+          </div>
+          <div className="sizes">
+            <h3>Sélectionne la quantité</h3>
+            <ul>
+              <li className="size">1</li>
+              <li className="size">2</li>
+              <li className="size">3</li>
+            </ul>
+          </div>
+          <button className="cart pop" type="button">
+            <span>Ajouter au panier</span>
+            <span>29.99€</span>
+          </button>
+        </div>
+      </div>
+      <div className="card js-tilt" data-tilt>
+        <div className="banner">
+          <h2>Fond de teint en Poudre</h2>
+        </div>
+        <img
+          src={foundation}
+          alt="Fond de teint en Poudre"
+          className="foundation"
+        />
+        <div className="content">
+          <div className="details">
+            <h3>Détails Produits</h3>
+            <small>Couleur: brun</small>
+            <small>durée: 8 semaines</small>
+          </div>
+          <div className="sizes">
+            <h3>Sélectionne la quantité</h3>
+            <ul>
+              <li className="size">1</li>
+              <li className="size">2</li>
+              <li className="size">3</li>
+            </ul>
+          </div>
+          <button className="cart pop" type="button">
+            <span>Ajouter au panier</span>
+            <span>29.99€</span>
+          </button>
+        </div>
+      </div>
+      <div className="card js-tilt" data-tilt>
+        <div className="banner">
+          <h2>Fond de teint en Poudre</h2>
+        </div>
+        <img
+          src={foundation}
+          alt="Fond de teint en Poudre"
+          className="foundation"
+        />
+        <div className="content">
+          <div className="details">
+            <h3>Détails Produits</h3>
+            <small>Couleur: brun</small>
+            <small>durée: 8 semaines</small>
+          </div>
+          <div className="sizes">
+            <h3>Sélectionne la quantité</h3>
+            <ul>
+              <li className="size">1</li>
+              <li className="size">2</li>
+              <li className="size">3</li>
+            </ul>
+          </div>
+          <button className="cart pop" type="button">
+            <span>Ajouter au panier</span>
+            <span>29.99€</span>
+          </button>
+        </div>
+      </div>
+      <div className="card js-tilt" data-tilt>
+        <div className="banner">
+          <h2>Fond de teint en Poudre</h2>
+        </div>
+        <img
+          src={foundation}
+          alt="Fond de teint en Poudre"
+          className="foundation"
+        />
+        <div className="content">
+          <div className="details">
+            <h3>Détails Produits</h3>
+            <small>Couleur: brun</small>
+            <small>durée: 8 semaines</small>
+          </div>
+          <div className="sizes">
+            <h3>Sélectionne la quantité</h3>
+            <ul>
+              <li className="size">1</li>
+              <li className="size">2</li>
+              <li className="size">3</li>
+            </ul>
+          </div>
+          <button className="cart pop" type="button">
+            <span>Ajouter au panier</span>
+            <span>29.99€</span>
+          </button>
+        </div>
+      </div>
+      <div className="card js-tilt" data-tilt>
+        <div className="banner">
+          <h2>Fond de teint en Poudre</h2>
+        </div>
+        <img
+          src={foundation}
+          alt="Fond de teint en Poudre"
+          className="foundation"
+        />
+        <div className="content">
+          <div className="details">
+            <h3>Détails Produits</h3>
+            <small>Couleur: brun</small>
+            <small>durée: 8 semaines</small>
           </div>
           <div className="sizes">
             <h3>Sélectionne la quantité</h3>
